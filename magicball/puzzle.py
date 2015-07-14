3fda186af621d7569fc1b94a142966f71b628c91
worldmaker <worldmaker18349276@gmail.com>
2015-07-14 22:56:18 +0800

    improve puzzle

--- magicball/puzzle.py
+++ magicball/puzzle.py
@@ -6,6 +6,7 @@ class IllegalOperationError(Exception):
 class IllegalStateError(Exception):
     pass
 
+
 class PuzzleSystem:
     def __init__(self, st, tr, func):
         if not hasattr(st, '__contains__'):
@@ -17,6 +18,8 @@ class PuzzleSystem:
         self.states = st
         self.transitions = tr
         self.applicationfunction = func
+    def __mul__(self, other): # tensor product
+        return TensorPuzzleSystem(self, other)
     def puzzle(pzlsystem):
         class Puzzle:
             def __init__(self, st):
@@ -32,11 +35,52 @@ class PuzzleSystem:
                 if st not in pzlsystem.states:
                     raise IllegalStateError
                 self.__state = st
-            def operate(self, trans):
+            def __mul__(self, trans):
                 if trans not in pzlsystem.transitions:
                     raise IllegalOperationError
                 self.state = pzlsystem.applicationfunction(self.state, trans)
-            def __mul__(self, trans):
-                self.operate(trans)
         return Puzzle
+
+class TensorPuzzleSystem(PuzzleSystem):
+    def __init__(self, *pzlsystems):
+        self.systems = pzlsystems
+    @property
+    def states(self):
+        AbstractTensorSet(*(pzlsys.states for pzlsys in self.systems))
+    @property
+    def transitions(self):
+        AbstractTensorSet(*(pzlsys.transitions for pzlsys in self.systems))
+    @property
+    def applicationfunction(self):
+        aps = tuple(pzlsys.applicationfunction for pzlsys in self.systems)
+        def tensorapplicationfunction(sts, trs):
+            return tuple(map(lambda ap, st, tr: ap(st, tr), aps, sts, trs))
+        return tensorapplicationfunction
+
+
+class AbstractSet:
+    def __init__(self, cond):
+        self.condition = cond
+    def __contains__(self, member):
+        return self.condition(member)
+    def __mul__(self, other): # tensor product
+        if hasattr(other, '__contains__'):
+            return AbstractTensorSet(self, other)
+        else:
+            raise ValueError
+
+class AbstractTensorSet(AbstractSet):
+    def __init__(self, *asets):
+        if not all(hasattr(aset, '__contains__') for aset in asets):
+            raise ValueError
+        self.sets = asets
+    def __contains__(self, members):
+        if len(self.sets) != len(members):
+            return False
+        return all(members in aset for aset in self.sets)
+    def __mul__(self, other): # tensor product
+        if hasattr(other, '__contains__'):
+            return AbstractTensorSet(*self.sets, other)
+        else:
+            raise ValueError
 
