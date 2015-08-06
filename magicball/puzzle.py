fd6f060bb0b7eb055c59d334a446b3708f1529c5
worldmaker <worldmaker18349276@gmail.com>
2015-08-06 22:21:28 +0800

    adjust puzzle

--- magicball/puzzle.py
+++ magicball/puzzle.py
@@ -10,26 +10,6 @@ class IllegalStateError(Exception):
 
 
 class PuzzleSystem:
-    def __init__(self, sts, eops, eap):
-        if not hasattr(sts, '__contains__'):
-            raise ValueError
-        if not hasattr(eops, '__contains__'):
-            raise ValueError
-        if not hasattr(eap, '__call__'):
-            raise ValueError
-        self.stateset = sts
-        self.extendedoperationset = eops
-        self.extendedapplication = eap
-    def __str__(self):
-        return '('+str(self.stateset)+', '+str(self.extendedoperationset)+', '+str(self.extendedapplication)+')'
-    def apply(self, st, eop):
-        if st not in self.stateset or eop not in self.extendedoperationset:
-            return None
-        if any(eap(st, eop[:t+1]) not in rsts for t in range(len(eop))):
-            return None
-        return eap(st, eop)
-
-class DiscretePuzzleSystem(PuzzleSystem):
     def __init__(self, sts, ops, ap):
         if not hasattr(sts, '__contains__'):
             raise ValueError
@@ -37,20 +17,40 @@ class DiscretePuzzleSystem(PuzzleSystem):
             raise ValueError
         if not hasattr(ap, '__call__'):
             raise ValueError
-        self.stateset = sts
-        self.operationset = ops
+        self.states = sts
+        self.operations = ops
         self.application = ap
-        self.extendedoperationset = FreeMonoid(ops)
-        self.extendedapplication = lambda st, eop: reduce(ap, eop, st)
+    def __str__(self):
+        return '('+str(self.states)+', '+str(self.operations)+', '+str(self.application)+')'
+    def apply(self, st, op):
+        if st not in self.states or op not in self.operations:
+            return None
+        if any(self.application(st, op[:t+1]) not in self.states for t in range(len(op))):
+            return None
+        return self.application(st, op)
+
+class DiscretePuzzleSystem(PuzzleSystem):
+    def __init__(self, sts, pops, pap):
+        if not hasattr(sts, '__contains__'):
+            raise ValueError
+        if not hasattr(pops, '__contains__'):
+            raise ValueError
+        if not hasattr(pap, '__call__'):
+            raise ValueError
+        self.states = sts
+        self.primaryoperations = pops
+        self.primaryapplication = pap
+        self.operations = FreeMonoid(pops)
+        self.application = lambda st, op: reduce(pap, op, st)
     def tensor(pzlsystems):
         pzlsystems = tuple(pzlsystems)
         if any(not isinstance(pzlsys, DiscretePuzzleSystem) for pzlsys in pzlsystems):
             raise TypeError
-        stls = AbstractSet.tensor(pzlsys.stateset for pzlsys in pzlsystems)
-        opls = AbstractSet.tensor(pzlsys.operationset for pzlsys in pzlsystems)
-        apl = tuple(pzlsys.application for pzlsys in pzlsystems)
-        lap = lambda stl, opl: tuple(map(lambda ap, st, op: ap(st, op), apl, stl, opl))
-        return DiscretePuzzleSystem(stls, opls, lap)
+        stls = AbstractSet.tensor(pzlsys.states for pzlsys in pzlsystems)
+        popls = AbstractSet.tensor(pzlsys.primaryoperations for pzlsys in pzlsystems)
+        papl = tuple(pzlsys.primaryapplication for pzlsys in pzlsystems)
+        lpap = lambda stl, popl: tuple(map(lambda pap, st, pop: pap(st, pop), papl, stl, popl))
+        return DiscretePuzzleSystem(stls, popls, lpap)
 
 class ContinuousPuzzleSystem(PuzzleSystem):
     def __init__(self, sts, bops, bap):
@@ -60,17 +60,17 @@ class ContinuousPuzzleSystem(PuzzleSystem):
             raise ValueError
         if not hasattr(bap, '__call__'):
             raise ValueError
-        self.stateset = sts
-        self.basedoperationset = bops
+        self.states = sts
+        self.basedoperations = bops
         self.basedapplication = bap
-        self.extendedoperationset = PathMonoid(bops)
-        self.extendedapplication = lambda st, eop: bap(st, eop(len(eop)))
+        self.operations = PathMonoid(bops)
+        self.application = lambda st, op: bap(st, op(len(op)))
     def tensor(pzlsystems):
         pzlsystems = tuple(pzlsystems)
         if any(not isinstance(pzlsys, ContinuousPuzzleSystem) for pzlsys in pzlsystems):
             raise TypeError
-        stls = AbstractSet.tensor(pzlsys.stateset for pzlsys in pzlsystems)
-        bopls = ContinuousPuzzleSystem.tensor(pzlsys.basedoperationset for pzlsys in pzlsystems)
+        stls = AbstractSet.tensor(pzlsys.states for pzlsys in pzlsystems)
+        bopls = AbstractSet.tensor(pzlsys.basedoperations for pzlsys in pzlsystems)
         bapl = tuple(pzlsys.basedapplication for pzlsys in pzlsystems)
         lbap = lambda stl, bopl: tuple(map(lambda bap, st, bop: bap(st, bop), bapl, stl, bopl))
         return ContinuousPuzzleSystem(stls, bopls, lbap)
