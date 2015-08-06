e5f1a3a5adcd1662d4264ecb0a2957dd2d036dc0
worldmaker <worldmaker18349276@gmail.com>
2015-08-06 15:36:52 +0800

    add path in mathplus

--- magicball/mathplus.py
+++ magicball/mathplus.py
@@ -21,7 +21,7 @@ class AbstractSet:
 
 class AbstractAndSet(AbstractSet):
     def __init__(self, asets):
-        self.sets = asets
+        self.sets = tuple(asets)
     def __contains__(self, member):
         return all(member in aset for aset in self.sets)
     def __and__(self, other):
@@ -34,10 +34,10 @@ class AbstractAndSet(AbstractSet):
 
 class AbstractOrSet(AbstractSet):
     def __init__(self, asets):
-        self.sets = asets
+        self.sets = tuple(asets)
     def __contains__(self, member):
         return all(member in aset for aset in self.sets)
-    def __and__(self, other):
+    def __or__(self, other):
         if isinstance(other, AbstractOrSet):
             return AbstractOrSet(self.sets + other.sets)
         else:
@@ -62,4 +62,35 @@ class AbstractTensorSet(AbstractSet):
         return self.__contains__
     def __str__(self):
         return '('+'*'.join(str(aset) for aset in self.sets)+')'
+
+
+class Path:
+    def __init__( self, func, flen ):
+        self.function = func
+        self.length = flen
+    def __len__( self ):
+        return self.length
+    def __add__( self, other ):
+        if isinstance(other, Path):
+            func1 = self.function
+            func2 = other.function
+            flen1 = self.length
+            flen2 = other.length
+            return Path(lambda t: func1(flen1)*func2(t-flen1) if t>flen1 else func1(t), flen1+flen2)
+        else:
+            raise ValueError
+    def __getitem__( self, key ):
+        if not isinstance(key, slice):
+            raise ValueError
+        stp = key.stop or self.length
+        strt = key.start or 0
+        if stp not in range(self.length) or strt not in range(self.length) or stp < strt:
+            raise ValueError
+        func = self.function
+        return Path(lambda t: func(strt+t), stp-strt)
+    def __call__( self, t ):
+        if t in range(self.length):
+            return self.function(t)
+        else:
+            raise ValueError
 
