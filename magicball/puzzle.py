4af66c8c0a0b3c8cc726e2cbef41de6b28b66e72
worldmaker <worldmaker18349276@gmail.com>
2015-08-06 16:42:18 +0800

    simplify puzzle

--- magicball/puzzle.py
+++ magicball/puzzle.py
@@ -10,24 +10,6 @@ class IllegalStateError(Exception):
 
 
 class PuzzleSystem:
-    def __init__(self, sts, ops, ap):
-        if not hasattr(sts, '__contains__'):
-            raise ValueError
-        if not hasattr(ops, '__contains__'):
-            raise ValueError
-        if not hasattr(ap, '__call__'):
-            raise ValueError
-        self.stateset = sts
-        self.operationset = ops
-        self.application = ap
-    def __str__(self):
-        return '('+str(self.stateset)+', '+str(self.operationset)+', '+str(self.application)+')'
-    def operation(self, *args, **kwargs):
-        return Operation(self, *args, **kwargs)
-    def puzzle(self, *args, **kwargs):
-        return Puzzle(self, *args, **kwargs)
-
-class ContinuousPuzzleSystem(PuzzleSystem):
     def __init__(self, sts, eops, eap):
         if not hasattr(sts, '__contains__'):
             raise ValueError
@@ -38,49 +20,10 @@ class ContinuousPuzzleSystem(PuzzleSystem):
         self.stateset = sts
         self.extendedoperationset = eops
         self.extendedapplication = eap
-    @property
-    def operationset(self):
-        return self.extendedoperationset
-    @property
-    def application(self):
-        return self.extendedapplication
     def __str__(self):
         return '('+str(self.stateset)+', '+str(self.extendedoperationset)+', '+str(self.extendedapplication)+')'
-    def operation(self, *args, **kwargs):
-        return Operation(self, *args, **kwargs)
     def puzzle(self, *args, **kwargs):
         return Puzzle(self, *args, **kwargs)
-
-class Operation:
-    def __init__(self, pzlsys, ops):
-        self.system = pzlsys
-        self.operations = tuple(ops)
-    @property
-    def operations(self):
-        return self.__operations
-    @operations.setter
-    def operations(self, ops):
-        if any(op not in self.system.operationset for op in ops):
-            raise IllegalOperationError
-        self.__operations = ops
-    def append(self, op):
-        if op in self.system.operationset:
-            self.__operations = self.__operations + (op)
-        elif isinstance(op, Operation) and op.system == self.system:
-            self.__operations = self.__operations + op.__operations
-        else:
-            raise IllegalOperationError
-    def __mul__(self, op):
-        if op in self.system.operationset:
-            return self.system.operation(self.__operations + (op))
-        elif isinstance(op, Operation) and op.system == self.system:
-            return self.system.operation(self.__operations + op.__operations)
-        else:
-            raise IllegalOperationError
-    def __iter__(self):
-        return iter(self.operations)
-    def __str__(self):
-        return '*'.join(str(op) for op in self.operations)
 
 class Puzzle:
     def __init__(self, pzlsys, st):
@@ -94,23 +37,14 @@ class Puzzle:
         if st not in self.system.stateset:
             raise IllegalStateError
         self.__state = st
-    def operate(self, op):
-        if op in self.system.operationset:
-            self.state = self.system.application(self.state, op)
-        elif isinstance(op, Operation) and op.system == self.system:
-            for _ in map(self.operate, op):
-                pass
-        else:
+    def __imul__(self, op):
+        if op not in self.system.extendedoperationset:
             raise IllegalOperationError
+        self.state = self.system.extendedapplication(self.state, op)
     def __mul__(self, op):
-        if op in self.system.operationset:
-            return self.system.puzzle(self.system.application(self.state, op))
-        elif isinstance(op, Operation) and op.system == self.system:
-            pzl2 = self.system.puzzle(self.state)
-            pzl2.operate(op)
-            return pzl2
-        else:
+        if op not in self.system.extendedoperationset:
             raise IllegalOperationError
+        return self.system.puzzle(self.system.extendedapplication(self.state, op))
     def __str__(self):
         return str(self.state)
 
