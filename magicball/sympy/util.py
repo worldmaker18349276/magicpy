5ec544afdd98cc17dbe7128d2d105d75ff16164d
worldmaker <worldmaker18349276@gmail.com>
2015-08-28 17:42:44 +0800

    add toplus

--- magicball/sympy/util.py
+++ magicball/sympy/util.py
@@ -1,3 +1,4 @@
+import operator
 from sympy.core.containers import Tuple
 from sympy.core.symbol import Symbol
 from sympy.matrices.expressions.matexpr import MatrixSymbol
@@ -15,4 +16,26 @@ def is_Symbol(s):
 
 def is_Boolean(b):
     return getattr(b, 'is_Boolean', False) or getattr(b, 'is_Relational', False) or b in (true, false)
+
+
+def deep_iter(sequence, depth=-1, types=(list, tuple), iter=iter):
+    if not isinstance(sequence, types) or depth == 0:
+        return sequence
+    for elem in iter(sequence):
+        for e in deep_enum(elem, depth-1, types, iter):
+            yield e
+
+def deep_enum(sequence, depth=-1, types=(list, tuple), iter=iter):
+    if not isinstance(sequence, types) or depth == 0:
+        return (), sequence
+    n = 0
+    for elem in iter(sequence):
+        for k, e in deep_enum(elem, depth-1, types, iter):
+            yield (n,)+k, e
+        n += 1
+
+def deep_get(sequence, key, getter=operator.getitem):
+    if key is ():
+        return sequence
+    deep_get(getter(sequence, key[0]), key[1:], getter)
 
