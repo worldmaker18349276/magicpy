2887fabf3b64b286425f838a362f405e63c5173b
worldmaker <worldmaker18349276@gmail.com>
2015-09-02 20:01:05 +0800

    enhance relplus

--- magicball/symplus/relplus.py
+++ magicball/symplus/relplus.py
@@ -15,46 +15,72 @@ def ask_is_negative(term):
 def _is_positive(term):
     if getattr(term, 'is_number', False):
         return term > 0
+    elif isinstance(term, Pow):
+        if term.args[1]%2==1:
+            return _is_positive(term.args[0])
+        else:
+            return _is_positive(term.args[0]) or _is_negative(term.args[0])
+
     l = len(term.free_symbols)
     t = Poly(term).as_dict()
     if (all(c > 0 for c in t.values()) and
         all(i % 2 == 0 for d in t.keys() for i in d) and
         (0,)*l in t.keys()):
         return True
-    else:
-        return ask_is_positive(term)
+    return ask_is_positive(term)
 
 def _is_negative(term):
     if getattr(term, 'is_number', False):
         return term < 0
+    elif isinstance(term, Pow):
+        if term.args[1]%2==1:
+            return _is_negative(term.args[0])
+        else:
+            return False
+
     l = len(term.free_symbols)
     t = Poly(term).as_dict()
     if (all(c < 0 for c in t.values()) and
         all(i % 2 == 0 for d in t.keys() for i in d) and
         (0,)*l in t.keys()):
         return True
-    else:
-        return ask_is_negative(term)
+    return ask_is_negative(term)
 
 def _is_positive_or_zero(term):
     if getattr(term, 'is_number', False):
         return term >= 0
+    elif isinstance(term, Pow):
+        if term.args[1]%2==1:
+            return _is_positive_or_zero(term.args[0])
+        else:
+            return True
+
     t = Poly(term).as_dict()
     if (all(c > 0 for c in t.values()) and
         all(i % 2 == 0 for d in t.keys() for i in d)):
         return True
-    else:
-        return ask_is_positive(term)
+    return ask_is_positive(term)
 
 def _is_negative_or_zero(term):
     if getattr(term, 'is_number', False):
         return term <= 0
+    elif isinstance(term, Pow):
+        if term.args[1]%2==1:
+            return _is_negative_or_zero(term.args[0])
+
     t = Poly(term).as_dict()
     if (all(c < 0 for c in t.values()) and
         all(i % 2 == 0 for d in t.keys() for i in d)):
         return True
+    return ask_is_negative(term)
+
+def is_polynomial(expr):
+    try:
+        Poly(expr, expr.free_symbols)
+    except PolynomialError:
+        return False
     else:
-        return ask_is_negative(term)
+        return True
 
 def polyeqsimp(eq):
     """simplify poly equation/inequation and canonicalize it
@@ -76,10 +102,10 @@ def polyeqsimp(eq):
     2*x**2 + 6*y**2 + 4 > 0
     >>> polyeqsimp(_)
     True
-    >>> x**4+3*x**2*y**2 > 0
-    x**4 + 3*x**2*y**2 > 0
+    >>> x**4+3*x**2*y**3 > 0
+    x**4 + 3*x**2*y**3 > 0
     >>> polyeqsimp(_)
-    And(x != 0, x**2 + 3*y**2 > 0)
+    And(x != 0, x**2 + 3*y**3 > 0)
     >>> (x**2+1)*(2*x-y) > 0
     (2*x - y)*(x**2 + 1) > 0
     >>> polyeqsimp(_)
@@ -87,7 +113,7 @@ def polyeqsimp(eq):
     >>> eq = Gt((x**2+y**2)*(2*x-y), 0); eq
     (2*x - y)*(x**2 + y**2) > 0
     >>> polyeqsimp(eq)
-    Or(And(x - y/2 < 0, x**2 + y**2 < 0), And(x - y/2 > 0, x**2 + y**2 > 0))
+    And(x != 0, x - y/2 > 0, y != 0)
     >>> polyeqsimp(Gt(1,1, evaluate=False))
     False
     """
@@ -115,18 +141,36 @@ def polyeqsimp(eq):
     terms2 = map(lambda t: t.args[0], terms2)
     terms3 = filter(lambda t: t.args[1]%2==1, termspow)
     terms3 = map(lambda t: t.args[0], terms3)
-    terms1 = list(chain(terms1, terms3))
-    terms2 = list(terms2)
+
+    # find positive-or-zero terms
+    termsp_ = list(terms2)
+    termsn = []
+    for term in chain(terms1, terms3):
+        if _is_positive_or_zero(term):
+            termsp_.append(term)
+        elif _is_negative_or_zero(term):
+            termsp_.append(-term)
+            sign += 1
+        else:
+            termsn.append(term)
+
+    # reduce polynomial positive-or-zero terms
+    termsp = []
+    for term in termsp_:
+        if is_polynomial(term):
+            termsp.extend(term.free_symbols)
+        else:
+            termsp.append(term)
 
     # normalize leading coeff
-    for i in range(len(terms1)):
-        lc = LC(terms1[i])
+    for i in range(len(termsn)):
+        lc = LC(termsn[i])
         if lc < 0:
             sign += 1
-        terms1[i] = terms1[i]/lc
-    for i in range(len(terms2)):
-        lc = LC(terms2[i])
-        terms2[i] = terms2[i]/lc
+        termsn[i] = termsn[i]/lc
+    for i in range(len(termsp)):
+        lc = LC(termsp[i])
+        termsp[i] = termsp[i]/lc
 
     # swap relation
     if sign % 2 == 1:
@@ -135,42 +179,42 @@ def polyeqsimp(eq):
                 Gt: Lt, Ge: Le}
         rel = swap[rel]
 
-    # split relation
+    # construct expression
     eqs = []
     if rel == Ne:
-        eqs.append(And(*[Ne(term,0) for term in terms1+terms2]))
+        eqs.append(And(*[Ne(term,0) for term in termsn+termsp]))
     elif rel == Gt:
-        eq0 = And(*[Ne(term,0) for term in terms2])
-        for i in range(0, len(terms1)+1, 2):
-            for neg in combinations(terms1, i):
-                pos = tuple(set(terms1)-set(neg))
+        eq0 = And(*[Ne(term,0) for term in termsp])
+        for i in range(0, len(termsn)+1, 2):
+            for neg in combinations(termsn, i):
+                pos = tuple(set(termsn)-set(neg))
                 eqp = And(*[Gt(p,0) for p in pos])
                 eqn = And(*[Lt(n,0) for n in neg])
                 eqs.append(And(eq0, eqp, eqn))
     elif rel == Ge:
-        for i in range(0, len(terms1)+1, 2):
-            for neg in combinations(terms1, i):
-                pos = tuple(set(terms1)-set(neg))
+        for i in range(0, len(termsn)+1, 2):
+            for neg in combinations(termsn, i):
+                pos = tuple(set(termsn)-set(neg))
                 eqp = And(*[Gt(p,0) for p in pos])
                 eqn = And(*[Lt(n,0) for n in neg])
                 eqs.append(And(eqp, eqn))
     elif rel == Lt:
-        eq0 = And(*[Ne(term,0) for term in terms2])
-        for i in range(1, len(terms1)+1, 2):
-            for neg in combinations(terms1, i):
-                pos = tuple(set(terms1)-set(neg))
+        eq0 = And(*[Ne(term,0) for term in termsp])
+        for i in range(1, len(termsn)+1, 2):
+            for neg in combinations(termsn, i):
+                pos = tuple(set(termsn)-set(neg))
                 eqp = And(*[Gt(p,0) for p in pos])
                 eqn = And(*[Lt(n,0) for n in neg])
                 eqs.append(And(eq0, eqp, eqn))
     elif rel == Le:
-        for i in range(1, len(terms1)+1, 2):
-            for neg in combinations(terms1, i):
-                pos = tuple(set(terms1)-set(neg))
+        for i in range(1, len(termsn)+1, 2):
+            for neg in combinations(termsn, i):
+                pos = tuple(set(termsn)-set(neg))
                 eqp = And(*[Gt(p,0) for p in pos])
                 eqn = And(*[Lt(n,0) for n in neg])
                 eqs.append(And(eqp, eqn))
     if rel in (Eq, Ge, Le):
-        for term in terms1+terms2:
+        for term in termsn+termsp:
             eqs.append(Eq(term,0))
 
     return Or(*eqs)
