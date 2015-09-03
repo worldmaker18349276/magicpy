845876914bf743e1e4c0512caf327ced943986d3
worldmaker <worldmaker18349276@gmail.com>
2015-09-03 17:27:54 +0800

    add cut, nsetsimp in euclid

--- magicball/model/euclid.py
+++ magicball/model/euclid.py
@@ -1,7 +1,12 @@
-from sympy.core import sympify, Symbol
+from itertools import product
+from sympy.core import sympify, Symbol, Eq, Ne, Gt, Lt, Ge, Le
+from sympy.sets import Intersection, EmptySet
+from sympy.logic import Not
 from sympy.matrices.immutable import ImmutableMatrix as Mat
 from magicball.symplus.setplus import AbstractSet
+from magicball.symplus.relplus import logicrelsimp
 from magicball.symplus.matplus import *
+from magicball.num.sample import spsetsimp
 
 
 def halfspace(direction=[1,0,0], offset=0):
@@ -75,4 +80,72 @@ def revolution(func, direction=[1,0,0]):
     direction = normalize(direction)
     expr = func(dot(r, direction), norm(cross(r, direction)))
     return AbstractSet((x,y,z), expr)
+
+
+def complement(aset):
+    return AbstractSet(aset.variables, Not(aset.expr))
+
+def cut(asets, *knives):
+    """
+    >>> cube2x2x2 = cut({sphere()}, halfspace(i), halfspace(j), halfspace(k))
+    >>> for part in sorted(str(p.doit().expr) for p in cube2x2x2): print(part)
+    And(x <= 0, x**2 + y**2 + z**2 < 1, y <= 0, z <= 0)
+    And(x <= 0, x**2 + y**2 + z**2 < 1, y <= 0, z > 0)
+    And(x <= 0, x**2 + y**2 + z**2 < 1, y > 0, z <= 0)
+    And(x <= 0, x**2 + y**2 + z**2 < 1, y > 0, z > 0)
+    And(x > 0, x**2 + y**2 + z**2 < 1, y <= 0, z <= 0)
+    And(x > 0, x**2 + y**2 + z**2 < 1, y <= 0, z > 0)
+    And(x > 0, x**2 + y**2 + z**2 < 1, y > 0, z <= 0)
+    And(x > 0, x**2 + y**2 + z**2 < 1, y > 0, z > 0)
+    """
+    subspaces = tuple(zip(knives, map(complement, knives)))
+    cutted = set()
+    for sub in product(asets, *subspaces):
+        cutted.add(Intersection(*sub, evaluate=False))
+    return cutted
+
+def nsetsimp(sample, asets):
+    """
+    >>> from sympy import *
+    >>> knives = halfspace(i, 1), halfspace(j, 1), halfspace(-i, 1), halfspace(-j, 1)
+    >>> floppy3x3x1 = cut({sphere(3)}, *knives)
+    >>> for part in sorted(str(p.doit().expr) for p in floppy3x3x1): print(part)
+    And(-x <= 1, -y <= 1, x <= 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x <= 1, -y <= 1, x <= 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x <= 1, -y <= 1, x > 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x <= 1, -y <= 1, x > 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x <= 1, -y > 1, x <= 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x <= 1, -y > 1, x <= 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x <= 1, -y > 1, x > 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x <= 1, -y > 1, x > 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x > 1, -y <= 1, x <= 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x > 1, -y <= 1, x <= 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x > 1, -y <= 1, x > 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x > 1, -y <= 1, x > 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x > 1, -y > 1, x <= 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x > 1, -y > 1, x <= 1, x**2 + y**2 + z**2 < 9, y > 1)
+    And(-x > 1, -y > 1, x > 1, x**2 + y**2 + z**2 < 9, y <= 1)
+    And(-x > 1, -y > 1, x > 1, x**2 + y**2 + z**2 < 9, y > 1)
+    >>> floppy3x3x1 = nsetsimp(cube_sample(4, 5), floppy3x3x1)
+    >>> for part in sorted(str(p.doit().expr) for p in floppy3x3x1): print(part)
+    And(x + 1 < 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 < 0)
+    And(x + 1 < 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 >= 0, y - 1 <= 0)
+    And(x + 1 < 0, x**2 + y**2 + z**2 - 9 < 0, y - 1 > 0)
+    And(x + 1 >= 0, x - 1 <= 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 < 0)
+    And(x + 1 >= 0, x - 1 <= 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 >= 0, y - 1 <= 0)
+    And(x + 1 >= 0, x - 1 <= 0, x**2 + y**2 + z**2 - 9 < 0, y - 1 > 0)
+    And(x - 1 > 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 < 0)
+    And(x - 1 > 0, x**2 + y**2 + z**2 - 9 < 0, y + 1 >= 0, y - 1 <= 0)
+    And(x - 1 > 0, x**2 + y**2 + z**2 - 9 < 0, y - 1 > 0)
+    """
+    simplified = set()
+    for aset in asets:
+        aset = aset.doit()
+        expr = logicrelsimp(aset.expr)
+        aset = AbstractSet(aset.variables, expr)
+        aset = aset.expand()
+        aset = spsetsimp(sample, aset)
+        if aset != EmptySet():
+            simplified.add(aset)
+    return simplified
 
