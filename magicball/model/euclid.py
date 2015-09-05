9bdc03e1f5d9d06bcbadca2248e4b9c6c4203d96
worldmaker <worldmaker18349276@gmail.com>
2015-09-05 15:53:42 +0800

    add platonic solids in euclid

--- magicball/model/euclid.py
+++ magicball/model/euclid.py
@@ -1,4 +1,4 @@
-from sympy.core import sympify, Symbol
+from sympy.core import sympify, Symbol, S
 from sympy.sets import Intersection, EmptySet
 from sympy.logic import Not
 from sympy.matrices.immutable import ImmutableMatrix as Mat
@@ -95,4 +95,80 @@ def complement(aset):
 
 def with_complement(aset):
     return (aset, complement(aset))
+
+
+phi = S.GoldenRatio
+vertices_tetra = {
+    Mat([ 1, 1, 1]),
+    Mat([ 1,-1,-1]),
+    Mat([-1, 1,-1]),
+    Mat([-1,-1, 1])}
+vertices_octa = {
+    Mat([ 0, 0, 1]),
+    Mat([ 0, 0,-1]),
+    Mat([ 0, 1, 0]),
+    Mat([ 0,-1, 0]),
+    Mat([ 1, 0, 0]),
+    Mat([-1, 0, 0])}
+vertices_cube = {
+    Mat([ 1, 1, 1]),
+    Mat([ 1, 1,-1]),
+    Mat([ 1,-1, 1]),
+    Mat([ 1,-1,-1]),
+    Mat([-1, 1, 1]),
+    Mat([-1, 1,-1]),
+    Mat([-1,-1, 1]),
+    Mat([-1,-1,-1])}
+vertices_icosa = {
+    Mat([ 0, 1, phi]),
+    Mat([ 0, 1,-phi]),
+    Mat([ 0,-1, phi]),
+    Mat([ 0,-1,-phi]),
+    Mat([ phi, 0, 1]),
+    Mat([-phi, 0, 1]),
+    Mat([ phi, 0,-1]),
+    Mat([-phi, 0,-1]),
+    Mat([ 1, phi, 0]),
+    Mat([ 1,-phi, 0]),
+    Mat([-1, phi, 0]),
+    Mat([-1,-phi, 0])}
+vertices_dodeca = {
+    Mat([ 1, 1, 1]),
+    Mat([ 1, 1,-1]),
+    Mat([ 1,-1, 1]),
+    Mat([ 1,-1,-1]),
+    Mat([-1, 1, 1]),
+    Mat([-1, 1,-1]),
+    Mat([-1,-1, 1]),
+    Mat([-1,-1,-1]),
+    Mat([ 0, phi,-1/phi]),
+    Mat([ 0, phi, 1/phi]),
+    Mat([ 0,-phi,-1/phi]),
+    Mat([ 0,-phi, 1/phi]),
+    Mat([-1/phi, 0, phi]),
+    Mat([ 1/phi, 0, phi]),
+    Mat([-1/phi, 0,-phi]),
+    Mat([ 1/phi, 0,-phi]),
+    Mat([ phi,-1/phi, 0]),
+    Mat([ phi, 1/phi, 0]),
+    Mat([-phi,-1/phi, 0]),
+    Mat([-phi, 1/phi, 0])}
+
+ru_tetra = sqrt(3)
+ru_octa = S.One
+ru_cube = sqrt(3)
+ru_icosa = sqrt(phi+2)
+ru_dodeca = sqrt(3)
+
+ri_tetra = 1/sqrt(3)
+ri_octa = 1/sqrt(3)
+ri_cube = S.One
+ri_icosa = sqrt(3)*(1+phi)
+ri_dodeca = sqrt(3+4*phi)
+
+tetrahedron = Intersection(*[halfspace(v, -ri_tetra, True) for v in vertices_tetra])
+octahedron = Intersection(*[halfspace(v, -ri_octa, True) for v in vertices_cube])
+cube = Intersection(*[halfspace(v, -ri_cube, True) for v in vertices_octa])
+icosahedron = Intersection(*[halfspace(v, -ri_icosa, True) for v in vertices_dodeca])
+dodecahedron = Intersection(*[halfspace(v, -ri_dodeca, True) for v in vertices_icosa])
 
