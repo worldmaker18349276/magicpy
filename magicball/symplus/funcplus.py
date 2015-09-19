94e35c67bf7a4128a453df730fff05acae2eed46
worldmaker <worldmaker18349276@gmail.com>
2015-09-19 16:13:13 +0800

    add inverse

--- magicball/symplus/funcplus.py
+++ magicball/symplus/funcplus.py
@@ -1,23 +1,122 @@
-from sympy.core import Lambda, Expr
+from functools import reduce
+from sympy.core import Lambda, Expr, Tuple
 from sympy.core.operations import AssocOp
+from sympy.solvers import solve
 from sympy.sets import Set
 from sympy.functions import Id
-from magicball.symplus.util import is_Tuple
-from functools import reduce
+from magicball.symplus.util import is_Tuple, rename_variables_in
+
+
+class Inverse(Lambda):
+    identity = Id
+
+    def __new__(cls, function, **kwargs):
+        if not isinstance(function, Lambda):
+            raise TypeError('function is not a Lambda: %s' % function)
+        return Expr.__new__(cls, function, **kwargs)
+
+    @classmethod
+    def eval(cls, func):
+        if func == identity:
+            return identity
+        elif isinstance(func, Inverse):
+            return func.function
+        elif isinstance(func, Compose):
+            return Compose(*[Inverse(f) for f in func.functions[::-1]])
+
+        if hasattr(func, '_inv'):
+            inv_func = func._inv()
+            if inv_func is not None:
+                return inv_func
+
+        return None
+
+    @property
+    def function(self):
+        return self._args[0]
+
+    @property
+    def variables(self):
+        expr = self.function.expr
+        return expr if is_Tuple(expr) else Tuple(expr)
+
+    @property
+    def expr(self):
+        var = self.function.variables
+        return var if len(var) > 1 else var[0]
+
+    @property
+    def free_symbols(self):
+        return self.function.free_symbols
+
+    def __call__(self, *args):
+        if len(args) != len(self.variables):
+            raise ValueError
+        solns = solve([expr - val for val, expr in zip(args, self.variables)],
+                      self.expr)
+        if isinstance(solns, dict):
+            return tuple(soln[var] for var, soln in zip(self.function.variables, solns))
+        else:
+            raise ValueError
+
+    def as_lambda(self):
+        var = symbols('a:%s' % len(self.variables))
+        var = rename_variables_in(var, self.free_symbols)
+        return Lambda(var, self(*var))
+
+    def _hashable_content(self):
+        return self._args
+
+    @property
+    def is_identity(self):
+        return self.function.is_identity
 
 
 class Compose(Lambda, AssocOp):
     identity = Id
 
-    def __new__(cls, *functions):
+    def __new__(cls, *functions, **kwargs):
         for function in functions:
             if not isinstance(function, Lambda):
                 raise TypeError('function is not a Lambda: %s' % function)
-        return AssocOp.__new__(cls, *functions)
+        obj = AssocOp.__new__(cls, *functions, **kwargs)
+
+        if kwargs.get('evaluate', global_evaluate[0]):
+            if isinstance(obj, Compose):
+                eval_obj = Compose.eval(*obj.args)
+                if eval_obj is not None:
+                    return eval_obj
+
+        return obj
+
+    @classmethod
+    def eval(cls, *args):
+        funcs = list(args)
+        i = 0
+        while i < len(funcs)-1:
+            if funcs[i] == Inverse(funcs[i+1]) or Inverse(funcs[i]) == funcs[i+1]:
+                funcs = funcs[:i] + funcs[i+2:]
+                i = max(i-1, 0)
+            elif hasattr(funcs[i], '_compose'):
+                comp_funcs = funcs[i]._compose(funcs[i+1])
+                if comp_funcs is not None:
+                    if comp_funcs == cls.identity:
+                        funcs = funcs[:i] + funcs[i+2:]
+                    else:
+                        funcs = funcs[:i] + [comp_funcs] + funcs[i+2:]
+                i = max(i-1, 0)
+            else:
+                i = i + 1
+
+        if funcs == args:
+            return False
+        else:
+            return Compose(*funcs, evaluate=False)
+
 
     @property
     def functions(self):
-        return self._args[0]
+        return self._args
 
     @property
     def variables(self):
@@ -36,7 +135,7 @@ class Compose(Lambda, AssocOp):
         apply_multivar = lambda a, f: f(*tuple_if_not(a))
         return reduce(apply_multivar, self.functions, args)
 
-    def doit(self):
+    def as_lambda(self):
         return Lambda(self.variables, self(*self.variables))
 
     def _hashable_content(self):
@@ -48,10 +147,10 @@ class Compose(Lambda, AssocOp):
 
 
 class Apply(Expr):
-    def __new__(cls, function, *arguments):
+    def __new__(cls, function, *arguments, **kwargs):
         if not isinstance(function, Lambda):
             raise TypeError('function is not a Lambda: %s' % function)
-        return Expr.__new__(cls, function, *arguments)
+        return Expr.__new__(cls, function, *arguments, **kwargs)
 
     @property
     def function(self):
@@ -61,17 +160,18 @@ class Apply(Expr):
     def arguments(self):
         return self._args[1:]
 
-    def doit(self):
+    def doit(self, **hints):
+        self = Basic.doit(self, **hints)
         return self.function(*self.arguments)
 
 
 class Image(Set):
-    def __new__(cls, function, set):
+    def __new__(cls, function, set, **kwargs):
         if not isinstance(function, Lambda):
             raise TypeError('function is not a Lambda: %s' % function)
         if not isinstance(set, Set):
             raise TypeError('set is not a Set: %s' % set)
-        return Set.__new__(cls, function, set)
+        return Set.__new__(cls, function, set, **kwargs)
 
     @property
     def function(self):
