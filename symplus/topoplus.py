ff456e19b4eab1d5430cb07ccf1757b68621699b
worldmaker <worldmaker18349276@gmail.com>
2016-03-19 14:06:30 +0800

    add Interior, Closure in topoplus

--- symplus/topoplus.py
+++ symplus/topoplus.py
@@ -1,6 +1,7 @@
 from sympy.core import S, Ne, Eq, Gt, Ge, Lt, Le, oo, symbols
+from sympy.core.evaluate import global_evaluate
 from sympy.logic import true, false, Or, And
-from sympy.sets import Set, Interval, Intersection, Union, Complement, ProductSet
+from sympy.sets import Set, Interval, Intersection, Union, Complement, ProductSet, FiniteSet
 from symplus.setplus import AbstractSet
 
 
@@ -36,7 +37,8 @@ def is_open(aset):
             return false
 
     elif isinstance(aset, (Intersection, Union)):
-        if all(is_open(s) == true for s in aset.args):
+
+        if And(*map(is_open, aset.args)) == true:
             return true
 
     elif isinstance(aset, Complement):
@@ -44,7 +46,7 @@ def is_open(aset):
             return true
 
     elif isinstance(aset, ProductSet):
-        return And(*[is_open(s) for s in aset.args])
+        return And(*map(is_open, aset.args))
 
     try:
         res = aset.is_open
@@ -87,7 +89,7 @@ def is_closed(aset):
             return false
 
     elif isinstance(aset, (Intersection, Union)):
-        if all(is_closed(s) == true for s in aset.args):
+        if And(*map(is_closed, aset.args)) == true:
             return true
 
     elif isinstance(aset, Complement):
@@ -95,7 +97,7 @@ def is_closed(aset):
             return true
 
     elif isinstance(aset, ProductSet):
-        return And(*[is_closed(s) for s in aset.args])
+        return And(*map(is_closed, aset.args))
 
     try:
         res = aset.is_closed
@@ -106,6 +108,168 @@ def is_closed(aset):
     except NotImplementedError:
         raise NotImplementedError
 
+class Interior(Set):
+    def __new__(cls, set, **kwargs):
+        evaluate = kwargs.pop('evaluate', global_evaluate[0])
+        if evaluate:
+            return Interior.reduce(set)
+        return Set.__new__(cls, set, **kwargs)
+
+    @staticmethod
+    def reduce(aset):
+        """
+        >>> from sympy import *
+        >>> from symplus.setplus import *
+        >>> Interior(Interval(0,1, False, False))
+        (0, 1)
+        >>> Interior(Interval(1,2, True, False))
+        (1, 2)
+        >>> Interior(Interval(1,oo, False, False))
+        (1, oo)
+        >>> Interior(Interval(1,2, False, False) * Interval(0,1, False, False))
+        (1, 2) x (0, 1)
+        >>> x,y = symbols('x y')
+        >>> Interior(AbstractSet(x, x**2<1))
+        AbstractSet(x, x**2 < 1)
+        >>> Interior(AbstractSet(x, (x<6)&(x>=4)))
+        AbstractSet(x, And(x < 6, x > 4))
+        >>> Interior(AbstractSet((x,y), x<=y))
+        AbstractSet((x, y), x < y)
+        """
+        if isinstance(aset, Interior):
+            return aset
+
+        elif isinstance(aset, Interval):
+            return Interval(aset.start, aset.end, left_open=True, right_open=True)
+
+        elif isinstance(aset, AbstractSet):
+            if isinstance(aset.expr, (Or, And)):
+                return Interior.reduce(aset.expand())
+            elif isinstance(aset.expr, Ne):
+                return aset
+            elif isinstance(aset.expr, Eq):
+                return AbstractSet(aset.variables, false)
+            elif isinstance(aset.expr, (Gt, Ge)):
+                return AbstractSet(aset.variables, Gt(*aset.expr.args))
+            elif isinstance(aset.expr, (Lt, Le)):
+                return AbstractSet(aset.variables, Lt(*aset.expr.args))
+
+        elif isinstance(aset, Intersection):
+            return aset.func(*map(Interior.reduce, aset.args))
+
+        elif isinstance(aset, Complement):
+            return aset.func(Interior.reduce(aset.args[0]), Closure.reduce(aset.args[1]))
+
+        elif isinstance(aset, ProductSet):
+            return aset.func(*map(Interior.reduce, aset.args))
+
+        try:
+            res = aset.interior
+            if res is not None:
+                return res
+            else:
+                return aset - aset.boundary
+        except NotImplementedError:
+            return Interior(aset, evaluate=False)
+
+    @property
+    def set(self):
+        return self.args[0]
+
+    @property
+    def is_open(self):
+        return true
+
+    @property
+    def is_closed(self):
+        return false
+
+    def _absolute_complement(self):
+        return Closure(AbsoluteComplement(self.set))
+
+class Closure(Set):
+    def __new__(cls, set, **kwargs):
+        evaluate = kwargs.pop('evaluate', global_evaluate[0])
+        if evaluate:
+            return Closure.reduce(set)
+        return Set.__new__(cls, set, **kwargs)
+
+    @staticmethod
+    def reduce(aset):
+        """
+        >>> from sympy import *
+        >>> from symplus.setplus import *
+        >>> Closure(Interval(0,1, False, False))
+        [0, 1]
+        >>> Closure(Interval(1,2, True, False))
+        [1, 2]
+        >>> Closure(Interval(1,oo, False, False))
+        [1, oo)
+        >>> Closure(Interval(1,2, False, False) * Interval(0,1, False, False))
+        [1, 2] x [0, 1]
+        >>> x,y = symbols('x y')
+        >>> Closure(AbstractSet(x, x**2<1))
+        AbstractSet(x, x**2 <= 1)
+        >>> Closure(AbstractSet(x, (x<4)|(x>=6)))
+        AbstractSet(x, Or(x <= 4, x >= 6))
+        >>> Closure(AbstractSet((x,y), x<=y))
+        AbstractSet((x, y), x <= y)
+        """
+        if isinstance(aset, Closure):
+            return aset
+
+        elif isinstance(aset, Interval):
+            return Interval(aset.start, aset.end, left_open=False, right_open=False)
+
+        elif isinstance(aset, AbstractSet):
+            if isinstance(aset.expr, (Or, And)):
+                return Closure.reduce(aset.expand())
+            elif isinstance(aset.expr, Eq):
+                return aset
+            elif isinstance(aset.expr, Ne):
+                return AbstractSet(aset.variables, true)
+            elif isinstance(aset.expr, (Ge, Gt)):
+                return AbstractSet(aset.variables, Ge(*aset.expr.args))
+            elif isinstance(aset.expr, (Le, Lt)):
+                return AbstractSet(aset.variables, Le(*aset.expr.args))
+
+        elif isinstance(aset, Union):
+            return aset.func(*map(Closure.reduce, aset.args))
+
+        elif isinstance(aset, ProductSet):
+            return aset.func(*map(Closure.reduce, aset.args))
+
+        try:
+            res = aset.closure
+            if res is not None:
+                return res
+            else:
+                return aset + aset.boundary
+        except NotImplementedError:
+            return None
+
+    @property
+    def set(self):
+        return self.args[0]
+
+    @property
+    def is_open(self):
+        return false
+
+    @property
+    def is_closed(self):
+        return true
+
+    def _absolute_complement(self):
+        return Interior(AbsoluteComplement(self.set))
+
+def AbsoluteComplement(aset):
+    return aset._absolute_complement()
+
+def Exterior(aset):
+    return Interior(AbsoluteComplement(aset))
+
+# topology(open set definition)
 
 class Topology(Set):
     @property
@@ -142,7 +306,7 @@ class Topology(Set):
     def is_regular_open_set(self, aset):
         return Eq(aset, self.closure_of(self.interior_of(aset)))
 
-class DiscreteTopology(Set):
+class DiscreteTopology(Topology):
     """
     >>> from sympy import *
     >>> from symplus.setplus import *
@@ -189,7 +353,7 @@ class DiscreteTopology(Set):
     def is_regular_open_set(self, aset):
         return true
 
-class NaturalTopology(Set):
+class NaturalTopology(Topology):
     """
     >>> from sympy import *
     >>> from symplus.setplus import *
@@ -219,10 +383,10 @@ class NaturalTopology(Set):
         raise aset.boundary
 
     def closure_of(self, aset):
-        return aset.closure
+        return Closure(aset)
 
     def interior_of(self, aset):
-        return aset.interior
+        return Interior(aset)
 
     def is_open_set(self, aset):
         return is_open(aset)
