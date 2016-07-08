56907c7430cc99249f38b267cbd8130a7cc777ae
worldmaker <worldmaker18349276@gmail.com>
2016-07-08 22:40:26 +0800

    add docstring to puzzles

--- magicpy/abstract/puzzles.py
+++ magicpy/abstract/puzzles.py
@@ -10,9 +10,17 @@ class IllegalStateError(Exception):
 
 class Puzzle:
     def is_valid_state(self):
+        """
+        True if this puzzle is in the valid state.
+        """
         return True
 
     def is_valid_operation(self, op):
+        """
+        True if op is in the valid operation for this puzzle.
+        it should be true if op is appliable for this puzzle.
+        but the inverse is not neccessary to be true; this is just a type test.
+        """
         if isinstance(op, IdentityOperation):
             return True
 
@@ -26,9 +34,17 @@ class Puzzle:
             return False
 
     def _is_valid_operation(self, op):
+        """
+        True if op is valid operation for this puzzle.
+        this is for elementary operation.
+        """
         return False
 
     def transform_by(self, op):
+        """
+        transform this puzzle by operation op.
+        op is not neccessary to be valid operation.
+        """
         if isinstance(op, IdentityOperation):
             return self
 
@@ -44,9 +60,18 @@ class Puzzle:
             raise TypeError
 
     def _transform_by(self, op):
+        """
+        transform this puzzle by operation op.
+        this is for elementary operation.
+        """
         return NotImplemented
 
     def apply(self, op):
+        """
+        apply op to this puzzle.
+        before applying, op should be valid operation;
+        after applying, this puzzle should be in the valid state.
+        """
         if not self.is_valid_operation(op):
             raise IllegalOperationError
 
@@ -65,6 +90,10 @@ class Puzzle:
             raise TypeError
 
     def _apply(self, op):
+        """
+        apply op to this puzzle.
+        this is for elementary operation.
+        """
         self = self._transform_by(op)
 
         if not self.is_valid_state():
@@ -73,12 +102,21 @@ class Puzzle:
         return self
 
 class Operation:
+    """
+    operation.
+    """
     pass
 
 class IdentityOperation(Operation):
+    """
+    operation which do nothing.
+    """
     pass
 
 class ConcatenatedOperation(Operation):
+    """
+    operation which apply specific operations sequentially.
+    """
     def __new__(cls, *ops):
         if any(not isinstance(op, Operation) for op in ops):
             raise TypeError
@@ -128,6 +166,9 @@ class ConcatenatedOperation(Operation):
 
 
 class TensorPuzzle(Puzzle, tuple):
+    """
+    tensor of multiple puzzles.
+    """
     def __new__(cls, pzls):
         if not all(isinstance(pzl_i, Puzzle) for pzl_i in pzls):
             raise TypeError
@@ -144,10 +185,16 @@ class TensorPuzzle(Puzzle, tuple):
         return type(self)(pzl_i.transform_by(op_i) for pzl_i, op_i in zip(self, op))
 
 class TensorOperation(Operation, tuple):
+    """
+    tensor of multiple operation for TensorPuzzle.
+    """
     pass
 
 
 class GeneralPuzzle(Puzzle):
+    """
+    implement some useful operation: ConditionalOperation, ContinuousOperation.
+    """
     density = 10
 
     def _transform_by(self, op):
@@ -158,6 +205,9 @@ class GeneralPuzzle(Puzzle):
             return NotImplemented
 
     def cond_op_to_op(self, cond_op):
+        """
+        convert ConditionalOperation to Operation.
+        """
         for cond, op_i in cond_op.items():
             if self._filter(cond):
                 return op_i
@@ -165,6 +215,9 @@ class GeneralPuzzle(Puzzle):
             raise IllegalOperationError
 
     def _filter(self, cond):
+        """
+        True if this puzzle is in the condition cond.
+        """
         return NotImplemented
 
     def _apply(self, op):
@@ -178,6 +231,9 @@ class GeneralPuzzle(Puzzle):
             return NotImplemented
 
     def cont_apply(self, op):
+        """
+        continuously apply op to this puzzle.
+        """
         for t in range(int(op.distance*self.density)):
             moved = self._transform_by(op.to(t/self.density))
             if not moved.is_valid_state():
@@ -190,25 +246,46 @@ class GeneralPuzzle(Puzzle):
         return self
 
 class ConditionalOperation(Operation, dict):
+    """
+    operation which operate case by case.
+    key is condition, value is Operation.
+    """
     pass
 
 class ContinuousOperation(Operation):
+    """
+    operation which operate continuously.
+    operation can be sliced to any distance.
+    """
     def distance(self):
+        """
+        total distance of this operation.
+        """
         return NotImplemented
 
-    def to(self, index):
+    def to(self, dis):
+        """
+        slice this operation by dis.
+        """
         return NotImplemented
 
 class ContinuousIdentityOperation(ContinuousOperation, IdentityOperation):
+    """
+    identity operation with distance.
+    this is for helping to compose ParallelOperation/ContinuousConditionalOperation in some case.
+    """
     def __init__(self, dis):
         self.distance = dis
 
-    def to(self, index):
-        if index > self.distance:
+    def to(self, dis):
+        if dis > self.distance:
             raise ValueError
-        return ContinuousIdentityOperation(index)
+        return ContinuousIdentityOperation(dis)
 
 class ParallelOperation(ContinuousOperation, TensorOperation):
+    """
+    continuous tensor operation.
+    """
     def __new__(cls, ops):
         if not all(isinstance(op_i, ContinuousOperation) for op_i in ops):
             raise TypeError
@@ -226,6 +303,9 @@ class ParallelOperation(ContinuousOperation, TensorOperation):
         return type(self)(op_i.to(dis) for op_i in self)
 
 class ContinuousConditionalOperation(ContinuousOperation, ConditionalOperation):
+    """
+    continuous conditional operation.
+    """
     def __new__(cls, ops):
         if not all(isinstance(op_i, ContinuousOperation) for op_i in ops.values()):
             raise TypeError
@@ -244,6 +324,9 @@ class ContinuousConditionalOperation(ContinuousOperation, ConditionalOperation):
 
 
 class CombinationalPuzzle(GeneralPuzzle, tuple):
+    """
+    puzzle composed by multiple elements.
+    """
     def _transform_by(self, op):
         if isinstance(op, SelectiveOperation):
             return self._transform_by_comb(self.sel_op_to_comb_op(op))
@@ -255,12 +338,21 @@ class CombinationalPuzzle(GeneralPuzzle, tuple):
             return TypeError
 
     def _transform_by_comb(self, op):
+        """
+        transform this puzzle by combinational operation op.
+        """
         return type(self)(self.elem_transform_by(elem, action) for elem, action in zip(self, op))
 
     def elem_transform_by(self, elem, action):
+        """
+        transform elements of this puzzle elem by action.
+        """
         return NotImplemented
 
     def sel_op_to_comb_op(self, op):
+        """
+        convert SelectiveOperation to CombinationalOperation.
+        """
         comb_op = []
         for elem in self:
             for select, action in op.items():
@@ -272,6 +364,9 @@ class CombinationalPuzzle(GeneralPuzzle, tuple):
         return op.comb_type(comb_op)
 
     def elem_filter(self, elem, select):
+        """
+        True if elem is in the selection select.
+        """
         return NotImplemented
 
     def _apply(self, op):
@@ -285,15 +380,29 @@ class CombinationalPuzzle(GeneralPuzzle, tuple):
             return NotImplemented
 
 class CombinationalOperation(Operation, tuple):
+    """
+    operation that operate elements of combinational puzzle seperatly.
+    element of operation is action.
+    """
     pass
 
 class SelectiveOperation(Operation, dict):
+    """
+    operation that operate selected elements of combinational puzzle.
+    key is selection, value is action.
+    """
     comb_type = CombinationalOperation
 
 class ContinuousCombinationalOperation(ContinuousOperation, CombinationalOperation):
+    """
+    continuous combinational operation.
+    """
     pass
 
 class ContinuousSelectiveOperation(ContinuousOperation, SelectiveOperation):
+    """
+    continuous selective operation.
+    """
     comb_type = ContinuousCombinationalOperation
 
 
