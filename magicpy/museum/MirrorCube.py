9c78e1f02ed2ca306ff6d3099c9a8ae046bf3aa9
worldmaker <worldmaker18349276@gmail.com>
2017-04-16 13:51:12 +0800

    fix MirrorCube

--- magicpy/museum/MirrorCube.py
+++ magicpy/museum/MirrorCube.py
@@ -5,7 +5,7 @@ from magicpy.puzzle.phy import *
 
 unit = sympify(2)/3
 offset = unit/7
-MirrorCube = SymbolicSO3PhysicalPuzzle([Box(center=[offset, 3*offset, 5*offset])])
+MirrorCube = SymbolicPhysicalPuzzle([Box(center=[offset, 3*offset, 5*offset])])
 MirrorCube = MirrorCube.cross_common([
      (Halfspace( unit, d), Halfspace(-unit, d)&Halfspace(-unit,-d), Halfspace( unit,-d))
      for d in [i,j,k]])
